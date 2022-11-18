bdef2648be166b22ac9bb0fe08ba367d225cde75
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2022-11-18 18:42:57 -0500

    Refactor to separate concerns
    - main.rs was doing too much, so we moved that responsibility to
    the parse_config function
    
    - parse_config was returning a tuple and that did not seem appropriate
    so moved that into a Config struct, which is more appropriate for
    holding configuration values meant for the program

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,30 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // it is convention to bring parent module into scope rather than the function
     let args: Vec<String> = env::args().collect();
-    let query_string = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Search for String: {}", query_string);
-    println!("In filename: {}", filename);
+    println!("Search for String: {}", config.query_string);
+    println!("In filename: {}", config.filename);
 
-    let contents = match fs::read_to_string(filename) {
+    let contents = match fs::read_to_string(config.filename) {
         Ok(file_contents) => file_contents,
         Err(error) => panic!("Error reading file contents {:?}", error)
     };
 
-    // let contents = fs::read_to_string(filename).expect("Something went wrong reading file");
-
     println!("With text: \n {}", contents);
+}
+
+struct Config {
+    query_string: String,
+    filename: String,
+}
+fn parse_config(args: &[String]) -> Config {
+    let query_string = args[1].clone();
+    let filename = args[2].clone();
+
+    Config {
+        query_string,
+        filename,
+    }
 }

40020c72c634822fe8c86ec8c16e7c9ef753f791
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2022-11-18 18:25:27 -0500

    Add file to grep query for

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // it is convention to bring parent module into scope rather than the function
@@ -8,4 +9,13 @@ fn main() {
 
     println!("Search for String: {}", query_string);
     println!("In filename: {}", filename);
+
+    let contents = match fs::read_to_string(filename) {
+        Ok(file_contents) => file_contents,
+        Err(error) => panic!("Error reading file contents {:?}", error)
+    };
+
+    // let contents = fs::read_to_string(filename).expect("Something went wrong reading file");
+
+    println!("With text: \n {}", contents);
 }

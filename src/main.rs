2e2c978af5dd535ba801c5ad208591c412dd200f
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2022-11-19 11:22:06 -0500

    Refactor modify main fn to handle Result enum
    - This refactor modifies the main fn to handle the Result enum that is
    returned by invoking Config::new() so that users of the program get
    better errors to reason about
    
    - After making this change on running the program with
    insuffucient arguments the program does not throw errors like
    RUST_BACKTRACE=1 and instead we get meaningful errors

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Search for String: {}", config.query_string);
     println!("In filename: {}", config.filename);
@@ -21,16 +25,16 @@ struct Config {
     filename: String,
 }
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments");
+            return Err("Not enough arguments");
         }
         let query_string = args[1].clone();
         let filename = args[2].clone();
 
-        Config {
+        Ok(Config {
             query_string,
             filename,
-        }
+        })
     }
 }

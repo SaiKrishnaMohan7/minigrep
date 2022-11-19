f33eb3401b2c6aa7aaa49bd0b42eb963bab745ee
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2022-11-19 11:39:19 -0500

    Refactor the `run` fn to return a Result enum
    - This refactor changes the run function to return the Result enum and
    modifies the `main` fn to handle the change made to `run` fn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,16 +13,19 @@ fn main() {
     println!("Search for String: {}", config.query_string);
     println!("In filename: {}", config.filename);
 
-    run(config);
+    if let Err(err) = run(config) {
+        println!("Application error: {}", err);
+
+        process::exit(1);
+    };
 }
 
-fn run(config: Config) {
-    let contents = match fs::read_to_string(config.filename) {
-        Ok(file_contents) => file_contents,
-        Err(error) => panic!("Error reading file contents {:?}", error)
-    };
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text: \n {}", contents);
+
+    Ok(())
 }
 
 struct Config {
